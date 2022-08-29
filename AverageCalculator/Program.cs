96fc70203a78d510463647398ff9a9106d197aec
root <root@localhost.localdomain>
2022-08-29 22:32:56 +0900

    wip: averagecalculator

---
+++ AverageCalculator/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace Checkpoint01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void InputID(int[] ID, int index)
+        {
+
+        }
+    }
+}
